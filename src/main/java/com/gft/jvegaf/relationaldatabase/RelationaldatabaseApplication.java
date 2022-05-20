cc2875d8891e8599b2dad6b92a0bd3f280bd6527
Jose Vega <Jose.Vega@gft.com>
2022-05-21 00:25:53 +0200

    Project boilerplate

---
+++ src/main/java/com/gft/jvegaf/relationaldatabase/RelationaldatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.gft.jvegaf.relationaldatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelationaldatabaseApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RelationaldatabaseApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/gft/jvegaf/relationaldatabase/RelationaldatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.jvegaf.relationaldatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelationaldatabaseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
